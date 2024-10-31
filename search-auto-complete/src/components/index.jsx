74dee2afb63bc331b65d75f8f1730608e839325f
firoliyos <firatesfaye44@gmail.com>
2024-10-31 05:50:06 +0300

    fetch-from-api-works

--- search-auto-complete/src/components/index.jsx
+++ search-auto-complete/src/components/index.jsx
@@ -1,9 +1,30 @@
-
+import {useState, useEffect} from 'react'
 
 export default function SearchAutoComplete(){
+    const [loading, setLoading] = useState(false)
+    const [users, setUsers] = useState([])
+    const [error, setError] = useState(null)
+
+    async function fetchUsers() {
+        try{
+          const response = await fetch('https://dummyjson.com/users')
+          const data = await response.json()
+          
+          console.log(data)
+        }catch(error) {
+            console.log(error)
+            setError(error)
+        }
+    }
+    useEffect(() => {
+        fetchUsers()
+    }, [])
     return(
-        <div>
-            
+        <div className="search-autocomplete-container">
+          <input
+           type="text"
+           placeholder="Search Users here..."
+           />
         </div>
     )
 }
