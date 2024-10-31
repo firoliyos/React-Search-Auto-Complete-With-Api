126b7982f71ca67eec1c8b14e7e59eb86261cdc9
firoliyos <firatesfaye44@gmail.com>
2024-10-31 06:32:40 +0300

    search-functionality-added

--- search-auto-complete/src/components/index.jsx
+++ search-auto-complete/src/components/index.jsx
@@ -4,14 +4,35 @@ export default function SearchAutoComplete(){
     const [loading, setLoading] = useState(false)
     const [users, setUsers] = useState([])
     const [error, setError] = useState(null)
+    const [searchParam, setSearchParam] = useState('')
+    const [showDropDown, setShowDropDown] = useState(false)
+    const [filteredUsers, setFilteredUsers] = useState([])
 
+    function handleOnChange(event) {
+        const query = event.target.value.toLowerCase()
+        setSearchParam(query)
+        if(query .length > 1) {
+            const filteredData = users && users.length ? 
+            users.filter((item) => item.toLowerCase().indexOf(query) > -1)
+            : []
+            setFilteredUsers(filteredData)
+            setShowDropDown(true)
+        } else {
+            setShowDropDown(false)
+        }
+    }
     async function fetchUsers() {
         try{
+            setLoading(true)
           const response = await fetch('https://dummyjson.com/users')
           const data = await response.json()
-          
-          console.log(data)
+          if(data && data.users && data.users.length) {
+            setLoading(false)
+            setUsers(data.users.map((userItem)=> userItem.firstName))
+            setError(false)
+          }
         }catch(error) {
+            setLoading(false)
             console.log(error)
             setError(error)
         }
@@ -19,11 +40,14 @@ export default function SearchAutoComplete(){
     useEffect(() => {
         fetchUsers()
     }, [])
+    console.log(users, filteredUsers)
     return(
         <div className="search-autocomplete-container">
           <input
+           value={searchParam}
            type="text"
            placeholder="Search Users here..."
+           onChange={handleOnChange}
            />
         </div>
     )
